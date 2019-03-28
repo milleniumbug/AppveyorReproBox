da77a0944269f9c5c29470a7de433605b81caa11
milleniumbug <milleniumbug@protonmail.com>
2019-03-28 18:30:33 +0100

    First commit

---
+++ UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Console.WriteLine("龠瀹爚禴龡龢籥鑰龣龥鸙籲");
+            Assert.AreEqual("龠", "瀹");
+        }
+    }
+}
